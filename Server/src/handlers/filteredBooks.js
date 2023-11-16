184467cc6fafa6075ccea46b0c26f995f6417fb0
JuanD <juanxd3009@gmail.com>
2023-11-16 16:54:13 -0500

    filtros por author, gender y year funcionando

---
+++ Server/src/handlers/filteredBooks.js
@@ -0,0 +1,29 @@
+const { Book } = require("../db");
+const { Op } = require("sequelize");
+
+const filteredBooks = async (author, gender, year) => {
+  const bookParams = {};
+  try {
+    if (author) {
+      const authorOk = author.trim();
+      bookParams.author = {
+        [Op.iLike]: `%${authorOk}%`,
+      };
+    }
+    if (gender) {
+      const genderOk = gender.trim();
+      bookParams.gender = genderOk;
+    }
+    if (year) {
+      const yearOk = year.trim();
+      bookParams.year = yearOk;
+    }
+    const books = await Book.findAll({ where: bookParams });
+    const filteredBooks = books.map((book) => book.dataValues);
+    return filteredBooks;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = filteredBooks;
--- Server/src/routes/Book.js
+++ Server/src/routes/Book.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const bookId = require("../handlers/bookId");
+const filteredBooks = require("../handlers/filteredBooks");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("GET BOOK");
+});
+
+router.get("/filter", async (req, res) => {
+  const { author, gender, year } = req.query;
+  try {
+    const filtered = await filteredBooks(author, gender, year);
+    res.status(200).json(filtered);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
 });
 
 router.get("/:id", async (req, res) => {
