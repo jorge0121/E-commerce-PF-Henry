2812139ec248a35ee09feb6a36fdd531447a76c0
jorge0121 <jorgeluisdiz@gmail.com>
2024-01-12 14:25:52 -0500

    prueba de otra BD

--- Server/src/db.js
+++ Server/src/db.js
@@ -10,7 +10,7 @@ const { USER, PASSWORD, HOST, PORT, BDD } = process.env;
 const { Sequelize } = require("sequelize");
 
 // Conexion
-const database = new Sequelize(`postgresql://postgres:FaB-A3GB3G3f-3Cf-D43-ccccF1*EggA@monorail.proxy.rlwy.net:30475/railway`,
+const database = new Sequelize(`postgresql://postgres:FaB-A3GB3G3f-3Cf-D43-ccccF1*EggA@postgres.railway.internal:5432/railway`,
   { logging: false,
     native: false, 
   dialectOptions: {
@@ -21,8 +21,9 @@ const database = new Sequelize(`postgresql://postgres:FaB-A3GB3G3f-3Cf-D43-ccccF
   },
   }
 );
-
+//postgresql://postgres:FaB-A3GB3G3f-3Cf-D43-ccccF1*EggA@monorail.proxy.rlwy.net:30475/railway
 //postgresql://postgres:-3EADg6-f3Df1g*bFDb4BC*23GeDbBag@viaduct.proxy.rlwy.net:49375/railway
+//postgresql://postgres:FaB-A3GB3G3f-3Cf-D43-ccccF1*EggA@postgres.railway.internal:5432/railway
 ModelsBook(database);
 ModelsUsers(database);
 ModelsComments(database);
